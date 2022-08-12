66134eee2c08df1191ec06c711470b42fe368a7d
HamaWhite <baisongxx@gmail.com>
2022-08-12 15:22:42 +0800

    add readme

--- src/main/java/com/dtwave/flink/lineage/LineageContext.java
+++ src/main/java/com/dtwave/flink/lineage/LineageContext.java
@@ -45,7 +45,7 @@ public class LineageContext {
 
     private static final Logger LOG = LoggerFactory.getLogger(LineageContext.class);
 
-    private static final String HIVE_CONF_DIR = "data";
+    private static final String HIVE_CONF_DIR = "conf";
 
     private final StreamExecutionEnvironment env;
     private final TableEnvironmentImpl tableEnv;
