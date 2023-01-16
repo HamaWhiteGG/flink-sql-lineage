c58301935cea43f72d5c8ba80228bd6bfa9812c1
HamaWhite <song.bs@dtwave-inc.com>
2023-01-16 17:45:47 +0800

    Support field lineage analysis of flink1.16

---
+++ lineage-flink1.16.x/src/test/java/com/hw/lineage/flink/SuiteTest.java
@@ -0,0 +1,33 @@
+package com.hw.lineage.flink;
+
+import com.hw.lineage.flink.cep.CepTest;
+import com.hw.lineage.flink.common.CommonTest;
+import com.hw.lineage.flink.localtimestamp.LocaltimestampTest;
+import com.hw.lineage.flink.lookup.join.LookupJoinTest;
+import com.hw.lineage.flink.tablefuncion.TableFunctionTest;
+import com.hw.lineage.flink.watermark.WatermarkTest;
+import com.hw.lineage.flink.window.WindowTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @description: SuiteTest
+ * @author: HamaWhite
+ * @version: 1.0.0
+ * @date: 2022/11/24 5:49 PM
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CommonTest.class
+        , LookupJoinTest.class
+        , TableFunctionTest.class
+        , WatermarkTest.class
+        , LocaltimestampTest.class
+        , CepTest.class
+        , WindowTest.class})
+public class SuiteTest {
+
+    /**
+     * The entry class of the test suite is just to organize the test classes together for testing,
+     * without any test methods.
+     */
+}
