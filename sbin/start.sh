147825c85c37b956fa03cb2eefb6576e6d9f4d2b
HamaWhite <song.bs@dtwave-inc.com>
2023-02-25 02:33:41 +0800

    add sbin/stop.sh, sbin/deploy.sh

--- sbin/start.sh
+++ sbin/start.sh
@@ -2,22 +2,27 @@
 
 APP_NAME=lineage-server
 
-cd `dirname $0`
-cd ..
-DEPLOY_DIR=`pwd`
+cd $(dirname "$0") || exit
+cd .. || exit
+DEPLOY_DIR=$(pwd)
 
 LOGS_DIR=${DEPLOY_DIR}/logs
-if [ ! -d ${LOGS_DIR} ]; then
-    mkdir ${LOGS_DIR}
+if [ ! -d "${LOGS_DIR}" ]; then
+    mkdir "${LOGS_DIR}"
 fi
-LOG_FILES=${LOGS_DIR}/lineage-server.log
+LOG_FILE=${LOGS_DIR}/lineage-server.log
 
 JAVA_OUT=${DEPLOY_DIR}/${APP_NAME}_std_out.log
 PID_FILE=${DEPLOY_DIR}/${APP_NAME}.pid
 
-JAVA_OPTS=" -Xmx2g -Xms2g"
-RUN_JAR=$(ls -t ${DEPLOY_DIR}/lineage-server/*.jar |head -n 1)
-echo "Starting the $SERVER_NAME ..."
-nohup java "$JAVA_OPTS" -jar "$RUN_JAR" & > $JAVA_OUT & echo $! > "$PID_FILE"
-sleep 5
-echo "Please check the log files: $LOG_FILES"
+RUN_JAR=$(ls -t "${DEPLOY_DIR}"/lineage-server/*.jar |head -n 1)
+echo "Starting the $APP_NAME ..."
+
+nohup java -Xmx2g -Xms2g -jar "$RUN_JAR" &> "$JAVA_OUT" & echo $! > "$PID_FILE"
+
+sleep 1
+
+printf "\n\n"
+echo "Please check the std_out files: $JAVA_OUT"
+echo "Please check the log files: $LOG_FILE"
+printf "\n"
