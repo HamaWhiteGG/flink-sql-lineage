59012f97e339fdf14817126614a405e601ea389c
HamaWhite <baisongxx@gmail.com>
2023-04-07 11:33:01 +0800

    Add findFunctionTasks api (Add function GroupConcat through Extending MyBatis Dynamic SQL)

--- lineage-server/lineage-server-domain/src/main/java/com/hw/lineage/server/domain/query/function/dto/FunctionTaskDTO.java
+++ lineage-server/lineage-server-domain/src/main/java/com/hw/lineage/server/domain/query/function/dto/FunctionTaskDTO.java
@@ -14,6 +14,8 @@ public class FunctionTaskDTO implements Serializable {
 
     private Long taskId;
 
+    private String taskName;
+
     private List<Long> sqlIdList;
 
     private Long createTime;
