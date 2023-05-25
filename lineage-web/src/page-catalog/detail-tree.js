3777c3346f7f29f1a3f863de13a6f971ec754a24
xing.yx <1024223327@qq.com>
2023-05-25 17:59:30 +0800

    feat：add catalog api

--- lineage-web/src/page-catalog/detail-tree.js
+++ lineage-web/src/page-catalog/detail-tree.js
@@ -1,9 +1,13 @@
 import React,{useState, useEffect} from 'react'
+import {Breadcrumb} from 'antd'
 
 const Cm = () => {
   return (
     <div className='left-tree-box'>
-      detail-tree
+      <Breadcrumb separator="<">
+        <Breadcrumb.Item href="#/catalog">Catalogs</Breadcrumb.Item>
+        {/* <Breadcrumb.Item>{taskDetail?.taskName || '--'}</Breadcrumb.Item> */}
+      </Breadcrumb>
     </div>
   )
 }
